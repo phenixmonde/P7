dfbdaaf8c4311465942594be4aa0e21d34714d8f
phenixmonde <103932412+phenixmonde@users.noreply.github.com>
2022-05-17 19:36:27 +0200

    Add files via upload

---
+++ API/P7_FastAPI.py
@@ -0,0 +1,107 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.wsgi import WSGIMiddleware
+from pyngrok import ngrok
+import nest_asyncio
+import uvicorn
+import pickle
+import pandas as pd
+from pydantic import BaseModel
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import typing as T
+import numpy as np
+from pydantic import BaseModel
+import flask
+from flask import request, jsonify
+from markupsafe import escape
+# import streamlit as st
+
+filename = './LGBMC332_model.sav'
+#filename = 'RFC_model.sav'
+model = pickle.load(open(filename, 'rb'))
+
+df_model = pd.read_csv('./df_model_mini.csv')
+X = df_model.iloc[:,2:]
+y = df_model.iloc[:,1]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+#preds = model.predict(X_test.iloc[0:2,:])
+preds = model.predict(df_model.iloc[0:2,2:])
+
+preds_proba = model.predict_proba(df_model.iloc[:,2:])
+
+class Client(BaseModel):
+    id_client: int
+
+#app = FastAPI()
+
+#@app.get('/')
+#def index():
+#    return {'message': 'Hello, World'}
+
+#@app.get('/{name}')
+#def get_name(name: str):
+#    return {'Welcome To Dashboard': f'{name}'}
+
+
+#@app.post('/predict')
+#def predict(data: Client):
+#	data = data.dict()
+#	id_client_received = data['id_client']
+#	#id_client_received = data
+#	print(id_client_received)
+	
+#	prediction = model.predict(df_model[df_model["SK_ID_CURR"] == id_client_received].iloc[:,2:])
+	
+#	if(prediction[0]==1):
+#		prediction= "Not Eligible"
+#	else:
+#		prediction= "Eligible"
+		
+#	print(type(prediction))
+	
+#	return {'prediction': prediction}
+
+app = flask.Flask(__name__)
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+	#id_client = id_client.dict()
+	#id_client_received = id_client['id_client']
+	id_client_received = int(request.args.get('id_client'))
+	#id_client_received = id_client_received.get('id_client_received')
+	#id_client_received = escape(id_client)
+	
+	prediction = model.predict(df_model[df_model["SK_ID_CURR"] == id_client_received].iloc[:,2:])
+	
+	if(prediction[0] == 1):
+		prediction = "Not Eligible"
+	else:
+		prediction = "Eligible"
+		
+	#return(prediction)
+	return jsonify(prediction)
+	
+@app.route("/predictProba0", methods=['POST'])
+def predictProba0():
+	id_client_received = int(request.args.get('id_client'))
+	
+	index_predsProba = df_model[df_model["SK_ID_CURR"]==id_client_received].index.values[0]
+	print(index_predsProba)
+	return jsonify(preds_proba[index_predsProba][0])
+	
+@app.route("/predictProba1", methods=['POST'])
+def predictProba1():
+	id_client_received = int(request.args.get('id_client'))
+	
+	index_predsProba = df_model[df_model["SK_ID_CURR"]==id_client_received].index.values[0]
+	print(index_predsProba)
+	return jsonify(preds_proba[index_predsProba][1])
+
+if __name__ == '__main__':
+    app.run()
+	
+#nest_asyncio.apply()
+#uvicorn.run(app, host='127.0.0.1', port=8080)
---
+++ DASHBOARD/dashboard.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import requests
+import pickle
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#ENV
+url_predict = 'http://127.0.0.1:5000/predict'
+url_predict_proba0 = 'http://127.0.0.1:5000/predictProba0'
+url_predict_proba1 = 'http://127.0.0.1:5000/predictProba1'
+
+dic_exp = pickle.load(open('./dictionnaire_explainer5.sav', 'rb'))
+df_store = pickle.load(open("./df_store_40", 'rb'))
+df_model = pd.read_csv('./df_model_mini.csv')
+
+#SIDEBAR
+id_client = st.sidebar.selectbox('Client', df_model.iloc[:,0])
+data = {'id_client': id_client}
+l_crucial_var = ["CNT_FAM_MEMBERS", "EXT_SOURCE_2", "NONLIVINGAREA_MODE",
+				"EXT_SOURCE_3", "REGION_POPULATION_RELATIVE"]
+st.sidebar.dataframe(df_model.loc[df_model["SK_ID_CURR"] == id_client, l_crucial_var].T)
+
+#CENTER
+st.title('Client knowledge Dashboard')
+
+#Prediction
+status = requests.post(url_predict, params=data)
+val_status = status.json()
+if status == "Eligible":
+	statusProba = requests.post(url_predict_proba1, params=data)
+else:
+	statusProba = requests.post(url_predict_proba0, params=data)
+
+val_statusProba = statusProba.json()
+
+st.header(str(val_status) + " à: "+ str(round(val_statusProba, 2)) + "%")
+
+
+st.components.v1.html(dic_exp[id_client], height=800)
+st.header("DATAVIZ")
+feature_client = st.selectbox('Variable', df_model.columns[2:])
+
+
+fig = plt.figure()
+
+
+plt.bar(df_store[feature_client].index.values, df_store[feature_client], color = "black")
+#plt.bar(df_store0[feature_client].index.values, df_store0[feature_client], color = "r")
+#plt.bar(df_store1[feature_client].index.values, df_store1[feature_client], color = "b")
+#plt.hist(df_store1[feature_client], color = "b")
+
+#df_store = pickle.load(open("./df_store", 'rb'))
+#plt.hist(df_store[feature_client], color = "r")
+plt.axvline(x = df_model[df_model["SK_ID_CURR"] == id_client][feature_client].values[0], color = 'g', linestyle = '-')
+#plt.axhline(y = df_mean_graph_zero[feature_client].values[0], color = 'r', linestyle = '-')
+#plt.axhline(y = df_mean_graph_un[feature_client].values[0], color = 'g', linestyle = '-')
+# creating subplots
+#fig, ax = plt.subplots()
+ 
+# plotting columns
+#ax = sns.barplot(df_store0[feature_client], color='b')
+#ax = sns.barplot(df_store1[feature_client], color='r')
+ 
+ 
+st.pyplot(fig)	
+
