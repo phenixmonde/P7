e8e4e08e3bd7d50f92683b10fe83bad00940c30a
phenixmonde <103932412+phenixmonde@users.noreply.github.com>
2022-05-31 09:25:12 +0200

    Update dashboard.py

--- DASHBOARD/dashboard.py
+++ DASHBOARD/dashboard.py
@@ -7,17 +7,28 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 #ENV
-url_predict = 'http://Mondonoke.pythonanywhere.com/predict'
-url_predict_proba0 = 'http://Mondonoke.pythonanywhere.com/predictProba0'
-url_predict_proba1 = 'http://Mondonoke.pythonanywhere.com/predictProba1'
+#url_predict = 'http://mondonoke.pythonanywhere.com/predict'
+#url_predict_proba0 = 'http://mondonoke.pythonanywhere.com/predictProba0'
+#url_predict_proba1 = 'http://mondonoke.pythonanywhere.com/predictProba1'
 
-dic_exp = pickle.load(open('./DASHBOARD/dictionnaire_explainer5.sav', 'rb'))
-df_store = pickle.load(open("./DASHBOARD/df_store_40", 'rb'))
-df_model = pd.read_csv('./DASHBOARD/df_model_mini.csv')
+#url_predict = 'http://127.0.0.1:8080/predict'
+#url_predict_proba0 = 'http://127.0.0.1:8080/predictProba0'
+#url_predict_proba1 = 'http://127.0.0.1:8080/predictProba1'
+
+url_predict = 'https://bl-p7-api.herokuapp.com/predict'
+url_predict_proba0 = 'https://bl-p7-api.herokuapp.com/predictProba0'
+url_predict_proba1 = 'https://bl-p7-api.herokuapp.com/predictProba1'
+
+
+dic_exp = pickle.load(open('./dictionnaire_explainer5.sav', 'rb'))
+#dic_exp = pickle.load(open('./dictionnaire_explainerRFC5.sav', 'rb'))
+df_store = pickle.load(open("./df_store_40", 'rb'))
+df_model = pd.read_csv('./df_model_mini.csv')
+
+
 
 #SIDEBAR
 id_client = st.sidebar.selectbox('Client', df_model.iloc[:,0])
-data = {'id_client': id_client}
 l_crucial_var = ["CNT_FAM_MEMBERS", "EXT_SOURCE_2", "NONLIVINGAREA_MODE",
 				"EXT_SOURCE_3", "REGION_POPULATION_RELATIVE"]
 st.sidebar.dataframe(df_model.loc[df_model["SK_ID_CURR"] == id_client, l_crucial_var].T)
@@ -25,22 +36,34 @@ st.sidebar.dataframe(df_model.loc[df_model["SK_ID_CURR"] == id_client, l_crucial
 #CENTER
 st.title('Client knowledge Dashboard')
 
+st.dataframe(df_model[df_model["SK_ID_CURR"] == id_client])
+#st.dataframe(df_model[df_model["SK_ID_CURR"] == id_client].iloc[:, 2:])
+data = {'id_client': id_client}
+
+
+
+
 #Prediction
-status = requests.post(url_predict, params=data)
+status = requests.post(url_predict, json=data)
 val_status = status.json()
-if status == "Eligible":
-	statusProba = requests.post(url_predict_proba1, params=data)
-else:
-	statusProba = requests.post(url_predict_proba0, params=data)
 
-val_statusProba = statusProba.json()
+if val_status['prediction'] == "Eligible":
+	statusProba = requests.post(url_predict_proba0, json=data)
+	val_statusProba = statusProba.json()
+elif val_status['prediction'] == "Not Eligible":
+	statusProba = requests.post(url_predict_proba1, json=data)
+	val_statusProba = statusProba.json()
+else: 
+	val_statusProba = "Error"
+	
 
-st.header(str(val_status) + " à: "+ str(round(val_statusProba, 2)) + "%")
-
+st.header(str(val_status['prediction']) + " à: "+ str(round(val_statusProba['prediction'], 2)) + "%")
+#st.header(str(val_status) + " à: "+ str(round(val_statusProba, 2)) + "%")
 
 st.components.v1.html(dic_exp[id_client], height=800)
 st.header("DATAVIZ")
 feature_client = st.selectbox('Variable', df_model.columns[2:])
+
 
 
 fig = plt.figure()
