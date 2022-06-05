a7a157cb3a193278ed935d0cb3e830ce563ef8a3
phenixmonde <103932412+phenixmonde@users.noreply.github.com>
2022-06-05 13:04:22 +0200

    Update P7_FastAPI.py

--- API/P7_FastAPI.py
+++ API/P7_FastAPI.py
@@ -7,28 +7,16 @@ import uvicorn
 import pickle
 import pandas as pd
 from pydantic import BaseModel
-#from sklearn.model_selection import train_test_split
-#from sklearn.ensemble import RandomForestClassifier
 import lightgbm
 import typing as T
 import numpy as np
 from pydantic import BaseModel
-#import flask
-#from flask import request, jsonify
 from markupsafe import escape
-# import streamlit as st
 
 filename = './API/LGBMC332_model.sav'
-#filename = './API/RFC_model200.sav'
 model = pickle.load(open(filename, 'rb'))
 
 df_model = pd.read_csv('./API/df_model_mini.csv')
-#X = df_model.iloc[:,2:]
-#y = df_model.iloc[:,1]
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
-
-#preds = model.predict(X_test.iloc[0:2,:])
-#preds = model.predict(df_model.iloc[0:2,2:])
 
 preds_proba = model.predict_proba(df_model.iloc[:,2:])
 
@@ -62,8 +50,6 @@ def predict(data: Client):
 	print(type(df_model[df_model["SK_ID_CURR"] == id_client_received].iloc[:,2:].squeeze().array))
 	
 	df_check = df_model[df_model["SK_ID_CURR"] == id_client_received].iloc[:,2:]
-	#df_check = df_model[df_model["SK_ID_CURR"] == id_client_received].iloc[:,2:].to_numpy()
-	#df_check = df_model[df_model["SK_ID_CURR"] == id_client_received].iloc[:,2:].squeeze().array.reshape(1, -1)
 	
 	#prediction = model.predict(df_model[df_model["SK_ID_CURR"] == id_client_received].iloc[:,2:])
 	prediction = model.predict(df_check)
@@ -97,44 +83,6 @@ def predictProba1(data: Client):
 	
 	return {'prediction': preds_proba[index_predsProba][1]}
 	#return {'prediction': model.predict_proba(df_model.iloc[index_predsProba,2:])[1]}
-
-#app = flask.Flask(__name__)
-
-#app.route('/')
-#def hello():
- #   return 'Hello, World!'
-
-#@app.route("/predict", methods=['POST'])
-#def predict():
-	#id_client = id_client.dict()
-	#id_client_received = id_client['id_client']
-#	id_client_received = int(request.args.get('id_client'))
-	#id_client_received = id_client_received.get('id_client_received')
-	#id_client_received = escape(id_client)
-	
-#	prediction = model.predict(df_model[df_model["SK_ID_CURR"] == id_client_received].iloc[:,2:])
-	
-#	if(prediction[0] == 1):
-#		prediction = "Not Eligible"
-#	else:
-#		prediction = "Eligible"
-		
-	#return(prediction)
-#	return jsonify(prediction)
-	
-#@app.route("/predictProba0", methods=['POST'])
-#def predictProba0():
-#	id_client_received = int(request.args.get('id_client'))
-	
-#	index_predsProba = df_model[df_model["SK_ID_CURR"]==id_client_received].index.values[0]
-#	return jsonify(preds_proba[index_predsProba][0])
-	
-#@app.route("/predictProba1", methods=['POST'])
-#def predictProba1():
-#	id_client_received = int(request.args.get('id_client'))
-	
-#	index_predsProba = df_model[df_model["SK_ID_CURR"]==id_client_received].index.values[0]
-#	return jsonify(preds_proba[index_predsProba][1])
 
 #if __name__ == '__main__':
 #    app.run()
