7c588b9e78a2e2150b63ced139dd49d266f744eb
phenixmonde <103932412+phenixmonde@users.noreply.github.com>
2022-06-05 13:06:23 +0200

    Update dashboard.py

--- DASHBOARD/dashboard.py
+++ DASHBOARD/dashboard.py
@@ -6,10 +6,6 @@ import pickle
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#ENV
-#url_predict = 'http://mondonoke.pythonanywhere.com/predict'
-#url_predict_proba0 = 'http://mondonoke.pythonanywhere.com/predictProba0'
-#url_predict_proba1 = 'http://mondonoke.pythonanywhere.com/predictProba1'
 
 #url_predict = 'http://127.0.0.1:8080/predict'
 #url_predict_proba0 = 'http://127.0.0.1:8080/predictProba0'
@@ -21,7 +17,6 @@ url_predict_proba1 = 'https://bl-p7-api.herokuapp.com/predictProba1'
 
 
 dic_exp = pickle.load(open('./DASHBOARD/dictionnaire_explainer5.sav', 'rb'))
-#dic_exp = pickle.load(open('./dictionnaire_explainerRFC5.sav', 'rb'))
 df_store = pickle.load(open("./DASHBOARD/df_store_40", 'rb'))
 df_model = pd.read_csv('./DASHBOARD/df_model_mini.csv')
 
@@ -58,7 +53,6 @@ else:
 	
 
 st.header(str(val_status['prediction']) + " à: "+ str(round(val_statusProba['prediction'], 2)) + "%")
-#st.header(str(val_status) + " à: "+ str(round(val_statusProba, 2)) + "%")
 
 st.components.v1.html(dic_exp[id_client], width=1500 , height=800)
 st.header("DATAVIZ")
@@ -72,22 +66,7 @@ a = df_store[feature_client].index
 b = df_store[feature_client]
 
 plt.bar(a, b, color = "black")
-#plt.bar(df_store0[feature_client].index.values, df_store0[feature_client], color = "r")
-#plt.bar(df_store1[feature_client].index.values, df_store1[feature_client], color = "b")
-#plt.hist(df_store1[feature_client], color = "b")
 
-#df_store = pickle.load(open("./df_store", 'rb'))
-#plt.hist(df_store[feature_client], color = "r")
 plt.axvline(x = df_model[df_model["SK_ID_CURR"] == id_client][feature_client].values[0], color = 'g', linestyle = '-')
-#plt.axhline(y = df_mean_graph_zero[feature_client].values[0], color = 'r', linestyle = '-')
-#plt.axhline(y = df_mean_graph_un[feature_client].values[0], color = 'g', linestyle = '-')
-# creating subplots
-#fig, ax = plt.subplots()
- 
-# plotting columns
-#ax = sns.barplot(df_store0[feature_client], color='b')
-#ax = sns.barplot(df_store1[feature_client], color='r')
- 
- 
 st.pyplot(fig)	
 
